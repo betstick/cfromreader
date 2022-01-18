314dc0f546cc9692fb178d5b43a84716641d2255
betstick <61669265+betstick@users.noreply.github.com>
2022-01-18 13:24:04 -0500

    initial bdf3

--- src/binders/bdf/bdf3.hpp
+++ src/binders/bdf/bdf3.hpp
@@ -7,14 +7,20 @@ namespace cfr
     {
         char version[8];
         FILE* data;
+        int size;
 
         BDF3(FILE* src)
         {
             this->data = src;
+
             char magic[4];
             fread(magic,1,4,src);
             assert(memcmp(magic,"BDF3",4));
+            
             fread(version,1,8,src);
+
+            fseek(src,0,SEEK_END);
+            this->size = ftell(src);
             fseek(src,0,SEEK_SET);
         };
     };
